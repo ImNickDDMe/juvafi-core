2f7a1e2b6ab1ecb287cf42019ec1df288c22daee
Nikolaos Papanastasis <nickpapanastasis9@gmail.com>
2024-07-07 14:47:48 +0300

    Hono & OpenAPI Setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { InterfaceToType } from 'hono/utils/types';
+import { OpenAPIHono } from '@hono/zod-openapi';
+import { SwaggerUI } from '@hono/swagger-ui';
+
+const app = new OpenAPIHono<{ Bindings: InterfaceToType<Env> }>();
+
+app.get('/', async (c) => {
+	return c.json('JuvaFi Core');
+});
+
+app.get('/docs', async (c) => {
+	return c.html(`
+		<html>
+			<head>
+        		<meta charset="utf-8" />
+        		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        		<meta name="description" content="SwaggerUI" />
+        		<title>JuvaFi Core - SwaggerUI</title>
+			</head>
+			<body>
+				<img src="https://i.ibb.co/JKVgH7h/logo.png" height="25%" />
+				${SwaggerUI({ url: '/swagger' })}
+			</body>
+		</html>
+	`);
+});
+
+app.doc('/swagger', {
+	openapi: '3.1.0',
+	info: {
+		version: '1.0.0',
+		title: 'JuvaFi Core'
+	}
+});
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return await app.fetch(request, env, ctx);
 	},
 } satisfies ExportedHandler<Env>;
