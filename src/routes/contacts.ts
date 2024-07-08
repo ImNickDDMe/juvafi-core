b1bf86a0bb79a22006ca225e2278dd0bb23971a7
Nikolaos Papanastasis <nickpapanastasis9@gmail.com>
2024-07-08 19:19:40 +0300

    Add POST Method

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -6,6 +6,7 @@ import { Contact } from '$schemas';
 const router = new OpenAPIHono<{ Bindings: InterfaceToType<Env> }>();
 
 // GET Method
+
 const getSchema = createRoute({
 	method: 'get',
 	path: '/{id}',
@@ -16,7 +17,7 @@ const getSchema = createRoute({
 	},
 	responses: {
 		200: {
-			description: 'Creates the contact with success.',
+			description: 'Returns the contact data with success.',
 			content: {
 				'application/json': {
 					schema: Contact,
@@ -51,7 +52,70 @@ router.openapi(getSchema, async (c) => {
 
 	if (!result) return c.json({ error: 'Nonexistent contact' }, 404);
 
-    return c.json(result, 200);
+	return c.json(result, 200);
+});
+
+// POST Method
+
+const postSchema = createRoute({
+	method: 'post',
+	path: '/',
+	request: {
+		body: {
+			content: {
+				'application/json': {
+					schema: Contact.omit({
+						id: true,
+					}),
+				},
+			},
+		},
+	},
+	responses: {
+		201: {
+			description: 'Inserts the contact data with success.',
+			content: {
+				'application/json': {
+					schema: Contact.pick({ id: true }),
+				},
+			},
+		},
+        400: {
+            description: 'Throws an error due to insufficient/invalid data.',
+            content: {
+                'application/json': {
+                    schema: ErrorSchema
+                }
+            }
+        },
+        500: {
+            description: 'Throws an error due to unknown conditions.',
+            content: {
+                'application/json': {
+                    schema: ErrorSchema
+                }
+            }
+        }
+	},
+});
+
+router.openapi(postSchema, async (c) => {
+    const data = c.req.valid('json');
+
+    const database = initClient(c.env.DATABASE_URL);
+
+    const result = await database.insertInto('contacts')
+        .values({
+            id: generateID('cnct'),
+            ...data
+        }).returning('id').executeTakeFirst();
+
+    if (!result) return c.json({ error: 'Unexpected error.' }, 500);
+
+    return c.json({ id: result.id }, 201);
+}, (result, c) => {
+    if (!result.success)
+        return c.json({ error: 'Insufficient or invalid data.' }, 400);
 });
 
 export default router;
